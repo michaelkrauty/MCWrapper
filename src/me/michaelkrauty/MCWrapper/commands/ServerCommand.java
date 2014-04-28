1c27d55938c1b42ee1b61ceeaff9b36ccd8f9140
michaelkrauty <michaelkrauty@gmail.com>
2014-04-27 18:37:59 -0700

    update

--- src/me/michaelkrauty/MCWrapper/commands/ServerCommand.java
+++ src/me/michaelkrauty/MCWrapper/commands/ServerCommand.java
@@ -1,9 +1,22 @@
 package me.michaelkrauty.MCWrapper.commands;
 
+import me.michaelkrauty.MCWrapper.Main;
+import me.michaelkrauty.MCWrapper.Server;
+
 public class ServerCommand {
 
 	public ServerCommand(String[] cmd) {
-		// TODO
+		String out = "";
+		int serverid = Integer.parseInt(cmd[0]);
+		for(int i = 1; i < cmd.length; i++){
+			if(i == cmd.length){
+				out = out + cmd[i];
+			}else{
+				out = out + cmd[i] + " ";
+			}
+		}
+		Server server = Main.wrapper.getServer(serverid);
+		server.executeCommand(out);
 	}
 
 }
