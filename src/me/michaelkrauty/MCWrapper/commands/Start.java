e1476054cb77a13c5a368382310dab3093433464
michaelkrauty <michaelkrauty@gmail.com>
2014-04-27 17:46:36 -0700

    update

--- src/me/michaelkrauty/MCWrapper/commands/Start.java
+++ src/me/michaelkrauty/MCWrapper/commands/Start.java
@@ -1,12 +1,11 @@
 package me.michaelkrauty.MCWrapper.commands;
 
-import me.michaelkrauty.MCWrapper.Main;
 import me.michaelkrauty.MCWrapper.Server;
 
 public class Start {
 
 	public Start(int serverid) {
-		Server server = Main.wrapper.getServer(serverid);
+		Server server = new Server(serverid);
 		server.start();
 	}
 }
