285628b5272c68b0bb0cdd28fd971977ba95b81a
michaelkrauty <michaelkrauty@gmail.com>
2014-04-28 23:40:10 -0700

    update

--- src/me/michaelkrauty/MCWrapper/ConnectionHandler.java
+++ src/me/michaelkrauty/MCWrapper/ConnectionHandler.java
@@ -1,28 +1,16 @@
 package me.michaelkrauty.MCWrapper;
 
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-
 public class ConnectionHandler implements Runnable {
 
-	public ConnectionHandler() {
+	private Thread t;
+	private String threadName;
 
-		System.out.println("Starting connection handler...");
+	ConnectionHandler() {
 
-		int portNumber = 3307;
-
-		try {
-			@SuppressWarnings("resource")
-			ServerSocket serverSocket = new ServerSocket(portNumber);
-			Socket clientSocket = serverSocket.accept();
-			new ClientSession(clientSocket);
-		} catch (IOException e) {
-			System.out
-					.println("Exception caught when trying to listen on port "
-							+ portNumber + " or listening for a connection");
-			System.out.println(e.getMessage());
-		}
 	}
 
+	@Override
+	public void run() {
+
+	}
 }
