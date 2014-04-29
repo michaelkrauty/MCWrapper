8fc0dd8c87d2a755b9ef16a1d05b9645b62947b5
michaelkrauty <michaelkrauty@gmail.com>
2014-04-29 01:12:04 -0700

    update

--- src/me/michaelkrauty/MCWrapper/ServerProtocol.java
+++ src/me/michaelkrauty/MCWrapper/ServerProtocol.java
@@ -5,21 +5,20 @@ public class ServerProtocol {
 	private int state = 0;
 
 	public String processInput(String theInput) {
-		String theOutput = null;
 
 		if (state == 0) {
-			theOutput = "test1";
 			state = 1;
+			return "test1";
 		}
 		if (state == 1) {
-			theOutput = "test2";
 			state = 2;
+			return "test2";
 		}
 		if (state == 2) {
-			theOutput = "test3";
 			state = 0;
+			return "test3";
 		}
-		return theOutput;
+		return "u wot m8";
 	}
 
 	@SuppressWarnings("unused")
