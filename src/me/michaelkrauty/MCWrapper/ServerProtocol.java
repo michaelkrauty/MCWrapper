9fafcb0835aa95f739878371d666294e4a585761
michaelkrauty <michaelkrauty@gmail.com>
2014-04-29 18:36:06 -0700

    update

--- src/me/michaelkrauty/MCWrapper/ServerProtocol.java
+++ src/me/michaelkrauty/MCWrapper/ServerProtocol.java
@@ -3,38 +3,35 @@ package me.michaelkrauty.MCWrapper;
 public class ServerProtocol {
 
 	private final int LOGIN = 0;
+	@SuppressWarnings("unused")
 	private final int POSTLOGIN = 1;
 
 	private boolean logged = false;
 
+	@SuppressWarnings("unused")
 	private int state = LOGIN;
 
 	public String processInput(String theInput) {
 
-		if (state == LOGIN && !logged) {
-			if (theInput != null) {
-				String[] input = theInput.split(",");
-				if (input.length == 2) {
-					if (checkLogin(input[0], input[1])) {
-						logged = true;
-						state = POSTLOGIN;
-						return "Logged in.";
-					} else {
-						return "Login failed!";
-					}
-				}
-			}
-			return null;
-		}
-		if (state == POSTLOGIN && logged) {
-			if (theInput != null) {
-				String[] input = theInput.split(",");
+		if (theInput != null) {
+			String[] input = theInput.split(" ");
+			if (logged) {
 				if (input.length == 1) {
 					if (input[0].equalsIgnoreCase("disconnect")) {
 						return "disconnect";
 					}
 					if (input[0].equalsIgnoreCase("help")) {
 						return "help is fun, I suppose";
+					}
+				}
+			} else {
+				if (input.length == 3) {
+					if (input[0].equalsIgnoreCase("login")
+							&& checkLogin(input[1], input[2])) {
+						logged = true;
+						return "Logged in.";
+					} else {
+						return "Login failed!";
 					}
 				}
 			}
