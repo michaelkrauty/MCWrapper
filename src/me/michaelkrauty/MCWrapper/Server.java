ea402657f2d66d561b02315f8290728d2eafbcbd
michaelkrauty <michaelkrauty@gmail.com>
2014-04-29 22:53:39 -0700

    update

--- src/me/michaelkrauty/MCWrapper/Server.java
+++ src/me/michaelkrauty/MCWrapper/Server.java
@@ -123,15 +123,18 @@ public class Server {
 	}
 
 	public void restart() {
-		if (isOnline()) {
-			stop();
-		}
+		stop();
 		while (isRunning()) {
 			try {
 				Thread.sleep(0);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
+		}
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
 		start();
 	}
