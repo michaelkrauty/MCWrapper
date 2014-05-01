a32086318760491461fa308b758545ecf123feaa
michaelkrauty <michaelkrauty@gmail.com>
2014-04-30 23:15:03 -0700

    update

--- src/me/michaelkrauty/MCWrapper/commands/StopAll.java
+++ src/me/michaelkrauty/MCWrapper/commands/StopAll.java
@@ -5,8 +5,8 @@ import me.michaelkrauty.MCWrapper.Main;
 public class StopAll {
 
 	public StopAll() {
-		for (int i = 0; i < Main.wrapper.getOnlineServers().length; i++) {
-			Main.wrapper.getServer(i).stop();
+		for (int i = 0; i < Main.servers.size(); i++) {
+			Main.servers.get(i).stop();
 		}
 	}
 }
