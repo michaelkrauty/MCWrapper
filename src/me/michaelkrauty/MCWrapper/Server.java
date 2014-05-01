a830d9e13a4a18a65d67a3b41adf96fc69b80b15
michaelkrauty <michaelkrauty@gmail.com>
2014-05-01 15:03:10 -0700

    update

--- src/me/michaelkrauty/MCWrapper/Server.java
+++ src/me/michaelkrauty/MCWrapper/Server.java
@@ -47,10 +47,11 @@ public class Server {
 			try {
 				ProcessBuilder pb = new ProcessBuilder();
 				pb.directory(new File(serverdir));
-				pb.command("su", "-c", "java -Xmx" + Integer.toString(memory)
-						+ "M -jar /home/mcwrapper/jar/test.jar --host " + host
-						+ " --port " + Integer.toString(port) + " nogui",
-						"mcwrapper_" + ownerid);
+				pb.command("su", "-m", "-c",
+						"java -Xmx" + Integer.toString(memory)
+								+ "M -jar /home/mcwrapper/jar/test.jar --host "
+								+ host + " --port " + Integer.toString(port)
+								+ " nogui", "mcwrapper_" + ownerid);
 				Process p = pb.start();
 				process = p;
 				try {
