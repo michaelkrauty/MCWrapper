21d4d41484a80f8a115078aaab662daf1f91121f
michaelkrauty <michaelkrauty@gmail.com>
2014-04-30 23:40:33 -0700

    update

--- src/me/michaelkrauty/MCWrapper/CreateUser.java
+++ src/me/michaelkrauty/MCWrapper/CreateUser.java
@@ -1,11 +1,14 @@
 package me.michaelkrauty.MCWrapper;
 
+import java.io.File;
 import java.io.IOException;
 
 public class CreateUser {
 
 	public CreateUser(int userid) {
 		try {
+			File userdir = new File("/home/mcwrapper/servers/" + userid);
+			userdir.mkdir();
 			ProcessBuilder pb = new ProcessBuilder(
 					"useradd -d /home/mcwrapper/servers/" + userid
 							+ " -s /usr/bin/rssh -G rsshusers mcwrapper_"
