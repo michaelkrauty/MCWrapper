ddc7ee81328828ce629043337d79a4f94dd24f7a
michaelkrauty <michaelkrauty@gmail.com>
2014-05-02 13:46:33 -0700

    update

--- src/me/michaelkrauty/MCWrapper/CreateUser.java
+++ src/me/michaelkrauty/MCWrapper/CreateUser.java
@@ -1,8 +1,5 @@
 package me.michaelkrauty.MCWrapper;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 public class CreateUser {
 
 	public CreateUser(int userid, String auth) {
@@ -10,24 +7,11 @@ public class CreateUser {
 			ProcessBuilder pb = new ProcessBuilder("sudo", "useradd", "-d",
 					"/home/mcwrapper/servers/" + userid, "-s", "/bin/bash",
 					"-G", "mcwrapper_users", "mcwrapper_" + userid);
-			Process p = pb.start();
-			BufferedReader in = new BufferedReader(new InputStreamReader(
-					p.getInputStream()));
-			String line;
-			while ((line = in.readLine()) != null) {
-				System.out.println(line);
-			}
+			pb.start();
+			System.out.println("User \"mcwrapper_" + userid + "\" Created.");
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 }
-
-/*
- * Process p = Runtime.getRuntime().exec(
- * "sudo useradd -d /home/mcwrapper/servers/" + userid +
- * " -s /bin/bash -G mcwrapper_users mcwrapper_" + userid); BufferedReader in =
- * new BufferedReader(new InputStreamReader( p.getInputStream())); String line;
- * while ((line = in.readLine()) != null) { System.out.println(line); }
- */
