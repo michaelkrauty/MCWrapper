8f99eb985397ae8be68528b48362803b598f9697
michaelkrauty <michaelkrauty@gmail.com>
2014-05-02 13:47:04 -0700

    update

--- src/me/michaelkrauty/MCWrapper/CreateUser.java
+++ src/me/michaelkrauty/MCWrapper/CreateUser.java
@@ -4,12 +4,10 @@ public class CreateUser {
 
 	public CreateUser(int userid, String auth) {
 		try {
-			ProcessBuilder pb = new ProcessBuilder("sudo", "useradd", "-d",
+			new ProcessBuilder("sudo", "useradd", "-d",
 					"/home/mcwrapper/servers/" + userid, "-s", "/bin/bash",
-					"-G", "mcwrapper_users", "mcwrapper_" + userid);
-			pb.start();
+					"-G", "mcwrapper_users", "mcwrapper_" + userid).start();
 			System.out.println("User \"mcwrapper_" + userid + "\" Created.");
-
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
