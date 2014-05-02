d3eb59eeb976821fe544bea1f8bd350928997c6d
michaelkrauty <michaelkrauty@gmail.com>
2014-05-01 22:24:46 -0700

    update

--- src/me/michaelkrauty/MCWrapper/CreateUser.java
+++ src/me/michaelkrauty/MCWrapper/CreateUser.java
@@ -1,5 +1,8 @@
 package me.michaelkrauty.MCWrapper;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class CreateUser {
 
 	public CreateUser(int userid, String auth) {
@@ -8,8 +11,13 @@ public class CreateUser {
 					"useradd -d /home/mcwrapper/servers/" + userid
 							+ " -s /bin/bash -G mcwrapper_users mcwrapper_"
 							+ userid);
-			pb.start();
-			System.out.println("Created user \"mcwrapper_" + userid + "\".");
+			Process p = pb.start();
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					p.getInputStream()));
+			String line;
+			while ((line = in.readLine()) != null) {
+				System.out.println(line);
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
