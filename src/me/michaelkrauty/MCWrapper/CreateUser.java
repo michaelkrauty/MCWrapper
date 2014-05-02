ec83316c947be98922c1ad62edbd7782410daa7f
michaelkrauty <michaelkrauty@gmail.com>
2014-05-02 13:42:37 -0700

    update

--- src/me/michaelkrauty/MCWrapper/CreateUser.java
+++ src/me/michaelkrauty/MCWrapper/CreateUser.java
@@ -7,21 +7,17 @@ public class CreateUser {
 
 	public CreateUser(int userid, String auth) {
 		try {
-			System.out.println("test1");
-			Process p = Runtime.getRuntime().exec("pwd");
-			System.out.println("test2");
+			Process p = Runtime.getRuntime().exec(
+					"sudo useradd -d /home/mcwrapper/servers/" + userid
+							+ " -s /bin/bash -G mcwrapper_users mcwrapper_"
+							+ userid);
 			BufferedReader in = new BufferedReader(new InputStreamReader(
 					p.getInputStream()));
-			System.out.println("test3");
 			String line;
-			System.out.println("test4");
 			while ((line = in.readLine()) != null) {
-				System.out.println("test5");
 				System.out.println(line);
 			}
-			System.out.println("test6");
 		} catch (Exception e) {
-			System.out.println("test7");
 			e.printStackTrace();
 		}
 	}
