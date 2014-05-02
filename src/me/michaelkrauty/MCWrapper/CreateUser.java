a37ef23daf134963e1fb0ef399f9bc9a6eba7239
michaelkrauty <michaelkrauty@gmail.com>
2014-05-02 12:47:36 -0700

    update

--- src/me/michaelkrauty/MCWrapper/CreateUser.java
+++ src/me/michaelkrauty/MCWrapper/CreateUser.java
@@ -7,10 +7,9 @@ public class CreateUser {
 
 	public CreateUser(int userid, String auth) {
 		try {
-			ProcessBuilder pb = new ProcessBuilder("sudo",
-					"useradd -d /home/mcwrapper/servers/" + userid
-							+ " -s /bin/bash -G mcwrapper_users mcwrapper_"
-							+ userid);
+			ProcessBuilder pb = new ProcessBuilder("sudo", "useradd", "-d",
+					"/home/mcwrapper/servers/" + userid, "-s", "/bin/bash",
+					"-G", "mcwrapper_users", "mcwrapper_" + userid);
 			Process p = pb.start();
 			BufferedReader in = new BufferedReader(new InputStreamReader(
 					p.getInputStream()));
