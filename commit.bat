a755cea161578a0db34249d5f3e2ee7f310f13e5
michaelkrauty <michaelkrauty@gmail.com>
2014-05-01 17:01:36 -0700

    update

--- commit.bat
+++ commit.bat
@@ -1,4 +1,9 @@
+if exist C:\Users\Michael\Dropbox\workspace2\MCWrapper (
 cd C:\Users\Michael\Dropbox\workspace2\MCWrapper
+)
+if exist ( C:\Users\Daniel\Documents\GitHub\MCWrapper
+cd C:\Users\Daniel\Documents\GitHub\MCWrapper
+)
 git pull
 git add --all
 git commit -m update
