b6fc27d0f9865bb501d4f24ec850491f629c9401
michaelkrauty <michaelkrauty@gmail.com>
2014-05-09 23:32:32 -0700

    Update ClientInput.java

--- src/me/michaelkrauty/MCWrapper/ClientConnection/ClientInput.java
+++ src/me/michaelkrauty/MCWrapper/ClientConnection/ClientInput.java
@@ -7,6 +7,7 @@ import java.net.Socket;
 
 public class ClientInput implements Runnable {
 
+	private Thread t;
 	private final Socket socket;
 
 	public ClientInput(Socket soc) {
@@ -14,7 +15,10 @@ public class ClientInput implements Runnable {
 	}
 
 	public void start() {
-
+		if (t == null) {
+			t = new Thread(this);
+			t.start();
+		}
 	}
 
 	@Override
