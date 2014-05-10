ab010bb7979687024b45f8e3ffcff9333a09b905
michaelkrauty <michaelkrauty@gmail.com>
2014-05-09 17:43:52 -0700

    update

--- src/me/michaelkrauty/MCWrapper/Server.java
+++ src/me/michaelkrauty/MCWrapper/Server.java
@@ -139,25 +139,6 @@ public class Server {
 		}
 	}
 
-	public void restart() {
-		// if (isRunning()) {
-		// stop();
-		// while (isRunning()) {
-		// try {
-		// Thread.sleep(0);
-		// } catch (InterruptedException e) {
-		// e.printStackTrace();
-		// }
-		// }
-		// try {
-		// Thread.sleep(500);
-		// } catch (InterruptedException e) {
-		// e.printStackTrace();
-		// }
-		// }
-		// start();
-	}
-
 	// get
 	public int getId() {
 		return id;
--- src/me/michaelkrauty/MCWrapper/commands/Restart.java
+++ src/me/michaelkrauty/MCWrapper/commands/Restart.java
@@ -1,10 +1,25 @@
 package me.michaelkrauty.MCWrapper.commands;
 
 import me.michaelkrauty.MCWrapper.Main;
+import me.michaelkrauty.MCWrapper.Server;
 
 public class Restart {
 
 	public Restart(int serverid) {
-		Main.wrapper.getServer(serverid).restart();
+		Server server;
+		if (Main.wrapper.getServer(serverid) != null) {
+			server = Main.wrapper.getServer(serverid);
+		} else {
+			server = new Server(serverid);
+		}
+		server.stop();
+		while (server.isRunning()) {
+			try {
+				Thread.sleep(0);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		server.start();
 	}
 }
