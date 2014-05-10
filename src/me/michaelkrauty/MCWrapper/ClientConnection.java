fad4c9fc4db98ab628c294a00478a90cc10d8f84
Daniel Oxenbury <daniel.oxenbury@hotmail.co.uk>
2014-05-10 02:06:23 +0100

    update

--- src/me/michaelkrauty/MCWrapper/ClientConnection.java
+++ src/me/michaelkrauty/MCWrapper/ClientConnection.java
@@ -1,19 +1,26 @@
 package me.michaelkrauty.MCWrapper;
 
+// Imports
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
+// Client Connection 
 public class ClientConnection implements Runnable {
+
+	// Create Socket Object
 	private final Socket socket;
+	// Create Thread Object
 	private Thread t;
 
+	// Constructor
 	ClientConnection(Socket soc) {
 		socket = soc;
 	}
 
+	// Run Method
 	public void run() {
 		try {
 			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
