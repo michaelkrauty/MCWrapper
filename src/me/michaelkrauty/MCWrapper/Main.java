f7f500cd622e45dd7538cf8920c43c30d7e987ae
michaelkrauty <michaelkrauty@gmail.com>
2014-05-11 12:38:51 -0700

    create logs dir if it doesn't exist

--- src/me/michaelkrauty/MCWrapper/Main.java
+++ src/me/michaelkrauty/MCWrapper/Main.java
@@ -1,6 +1,7 @@
 package me.michaelkrauty.MCWrapper;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -29,6 +30,10 @@ public class Main {
 		log.info("Initiating wrapper...");
 		log.info("Wrapper PID: " + wrapper.getPID());
 		new ConnectionHandler().start();
+		File logdir = new File("logs");
+		if (!logdir.exists()) {
+			logdir.mkdir();
+		}
 		log.info("done.");
 		// start main loop
 		mainLoop();
