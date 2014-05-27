487ed73ecea6bba95a24b33e11cbf4be2432865d
michaelkrauty <michaelkrauty@gmail.com>
2014-05-26 21:22:28 -0700

    reformat

--- src/main/java/me/michaelkrauty/MCWrapper/Commands/Start.java
+++ src/main/java/me/michaelkrauty/MCWrapper/Commands/Start.java
@@ -1,13 +1,6 @@
 package me.michaelkrauty.MCWrapper.Commands;
 
-import me.michaelkrauty.MCWrapper.Main;
-import me.michaelkrauty.MCWrapper.Server;
 import me.michaelkrauty.MCWrapper.ServerInstance;
-import org.apache.log4j.Logger;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class Start {
 
